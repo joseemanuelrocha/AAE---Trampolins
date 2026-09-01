3d4e0810d7d1f7e12b8640de734954a22e4b107a
joseemanuelrocha <joseemanuelrocha@gmail.com>
2026-09-01 11:09:52 +0100

    Refresh service worker cache for timer video update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aae-treino-v3-video-em-treino';
+const CACHE='aae-treino-v4-cronometro-com-video';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
