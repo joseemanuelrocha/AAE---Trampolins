ecca7535902d101192e8fcf633dea4eb81c41ee7
joseemanuelrocha <joseemanuelrocha@gmail.com>
2026-09-01 12:05:14 +0100

    Refresh cache for corrected trampoline plan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aae-treino-v6-preparar-video';
+const CACHE='aae-treino-v7-trampolim-corrigido';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
