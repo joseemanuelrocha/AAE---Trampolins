4ef2a19be9de425070e05e51b9963e722c0cb59a
joseemanuelrocha <joseemanuelrocha@gmail.com>
2026-09-02 11:48:41 +0100

    Refresh cache for flexibility and stretching update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aae-treino-v7-trampolim-corrigido';
+const CACHE='aae-treino-v11-flexibilidade-videos';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
